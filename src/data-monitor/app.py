4c537ac6308ac2fc318586585da7327ef28e6027
mibrl12 <mikalai.syty@gmail.com>
2019-02-05 23:37:02 +0100

    Adds initial implementation of data-monitor

---
+++ src/data-monitor/app.py
@@ -0,0 +1,15 @@
+import logging
+
+from common import config
+from reporter.reporter import Reporter
+
+log = logging.getLogger(__file__)
+
+
+def main():
+    log.info('Starting data-monitor')
+
+
+if __name__ == '__main__':
+    main()
+
