eec2f2005723332bdbc8a3d6b6cfe536da883698
mibrl12 <mikalai.syty@gmail.com>
2019-02-06 21:12:11 +0100

    Fixes typo in data-reporter config

--- src/data-reporter/src/common/config.py
+++ src/data-reporter/src/common/config.py
@@ -3,4 +3,4 @@ import os
 REPORTER_TYPE = os.environ.get('REPORTER_TYPE', 'PRICE')
 DATAHUB_BASE_URL = os.environ.get('DATAHUB_BASE_URL', 'http://127.0.0.1:8000')
 DATAHUB_API = os.environ.get('DATAHUB_API', 'api/v1/actuals')
-DELAY_SECONDS = int(os.environ.get('DELAY_SECONDS', '5'))
+DELAY_SECONDS = int(os.environ.get('DELAY_SECONDS', 5))
