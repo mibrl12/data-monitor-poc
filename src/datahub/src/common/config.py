8fc09d4e7a2d245dc3a1d8f424a5ec99165a7f7b
mibrl12 <mikalai.syty@gmail.com>
2019-02-06 17:54:41 +0100

    Makes datahub config settable from env vars

--- src/datahub/src/common/config.py
+++ src/datahub/src/common/config.py
@@ -1,2 +1,5 @@
-MONGO_DBNAME = 'actuals'
-MONGO_URL = 'mongodb://localhost:27017'
+import os
+
+
+MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'actuals')
+MONGO_URL = os.environ.get('MONGO_URL', 'mongodb://localhost:27017')
